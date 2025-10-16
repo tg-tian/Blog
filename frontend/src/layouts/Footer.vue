4440b7ee0228def29a66d9ea7083e1a0ad2fa3d4
tgapk <914837973@qq.com>
2025-10-16 14:15:54 +0800

    add:增加网站备案

--- frontend/src/layouts/Footer.vue
+++ frontend/src/layouts/Footer.vue
@@ -1,6 +1,15 @@
 <template>
     <footer class="text-black text-sm">
-        <p>© 2025 我的博客 - All rights reserved</p>
+        <p>
+            © 2025 我的博客 - All rights reserved
+            <span class="mx-2">·</span>
+            <a
+                href="https://beian.miit.gov.cn/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:underline"
+            >苏ICP备2025154768号-2</a>
+        </p>
     </footer>
 </template>
 
