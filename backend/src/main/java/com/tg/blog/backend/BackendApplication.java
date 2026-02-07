c5c276aedcfc11a2fca57bafac082c1c361e8860
tgapk <914837973@qq.com>
2026-02-07 18:06:50 +0800

    add:增加后端日志打印

--- backend/src/main/java/com/tg/blog/backend/BackendApplication.java
+++ backend/src/main/java/com/tg/blog/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.tg.blog.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,12 +13,14 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
  * @author TG
  * @since 1.0.0
  */
+@Slf4j
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @MapperScan("com.tg.blog.backend.dao")
 public class BackendApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        log.debug("博客系统后端启动成功!");
     }
     
 }
--- backend/src/main/java/com/tg/blog/backend/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tg/blog/backend/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.tg.blog.backend.common.exception.LoginBadCredentialsException;
 import com.tg.blog.backend.common.exception.LoginParameterException;
 import com.tg.blog.backend.common.exception.LoginServiceException;
 import com.tg.blog.backend.common.exception.LoginUserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,34 +17,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @author TG
  * @since 1.0.0
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     // 登录参数不合法：400
     @ExceptionHandler(LoginParameterException.class)
     public ResponseEntity<?> handleLoginParameter(LoginParameterException ex) {
+        log.warn("登录参数异常: {}", ex.getMessage());
         return ResponseEntity.failure(400, ex.getMessage());
     }
 
     // 用户不存在：404
     @ExceptionHandler(LoginUserNotFoundException.class)
     public ResponseEntity<?> handleLoginUserNotFound(LoginUserNotFoundException ex) {
+        log.warn("用户不存在: {}", ex.getMessage());
         return ResponseEntity.failure(404, ex.getMessage());
     }
 
     // 密码错误：401
     @ExceptionHandler(LoginBadCredentialsException.class)
     public ResponseEntity<?> handleLoginBadCredentials(LoginBadCredentialsException ex) {
+        log.warn("密码错误: {}", ex.getMessage());
         return ResponseEntity.failure(401, ex.getMessage());
     }
 
     // 登录服务异常：500
     @ExceptionHandler(LoginServiceException.class)
     public ResponseEntity<?> handleLoginService(LoginServiceException ex) {
+        log.error("登录服务异常", ex);
         return ResponseEntity.failure(500, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception ex) {
+        log.error("系统内部异常", ex);
         return ResponseEntity.failure(500, ex.getMessage());
     }
 }
